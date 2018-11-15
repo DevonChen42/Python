77728da6c753e28a85d60ec7357e6581a8e5d336
DevonChen42 <devonchen.cs@gmail.com>
2018-11-15 09:13:54 -0800

    moved stuff

---
+++ Date-and-Time/README.md
@@ -0,0 +1,2 @@
+# Date-and-Time
+This is only a template file, not my code
---
+++ Date-and-Time/demo-sleep.py
@@ -0,0 +1,7 @@
+import time
+print ("*** Hit CTRL+C to stop ***")
+
+while True:
+	print ("Current date & time " + time.strftime("%c"))
+	time.sleep(1)
+
