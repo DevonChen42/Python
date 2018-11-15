fb7ec5202f6c60fee74d48d40f88abf56ae879ae
DevonChen42 <devonchen.cs@gmail.com>
2018-11-15 08:55:59 -0800

    AA

--- list123.py
+++ list123.py
@@ -6,8 +6,8 @@ print("* * * * * * * * * * * * * * *")
 print("1 DIMENSIONAL LIST")
 oneD = [1,2,3,4,5,6,7,8,9,10,11,12]
 print (oneD)
-for n in range (0, len(oneD)):
-	print (oneD[n]," ", end="")
+for n in range (0,len(oneD)):
+	print(oneD[n]," ",end="")
 	
 print()
 print("* * * * * * * * * * * * * * *")
