57eaf2f041df0b43a3ec584fd115baef596e06fe
DevonChen42 <devonchen.cs@gmail.com>
2019-01-22 08:22:51 -0800

    quadratic

---
+++ quadratic/lambda1.py
@@ -0,0 +1,7 @@
+#create three different files using the lambda quadratic_function
+# * * * * * * * * * * * * * * * *
+# file 1 : lambda1.py
+x = lambda a, b, c: a + b + c
+fx = x(5,6,2)
+print(fx)
+
